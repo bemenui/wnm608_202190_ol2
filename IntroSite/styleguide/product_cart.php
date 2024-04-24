182d3899aaf4975b3471b8f12d3f36b98ddafbea
bemenui <bmendoza10@art.edu>
2024-04-23 23:44:46 -0700

    Create product_cart.php

---
+++ IntroSite/styleguide/product_cart.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	
+
+
+	<title>Cart</title>
+
+	<?php include "../parts/meta.php"; ?>
+	<? include "../parts/navbar.php"; ?>
+</head>
+<body>
+
+
+	<div class="container">
+
+		<div class="card soft">
+
+			<h2>Products in your Cart</h2>
+
+			<p>Ready for PlantTime?</p>
+
+			<p><a href="styleguide/product_checkout.php">Checkout</a></p>
+
+		</div>
+	</div>
+	
+</body>
+</html>
