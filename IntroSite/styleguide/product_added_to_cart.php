3b062c2f9b0b26b9301cec83dc901ca9070cc06a
bemenui <bmendoza10@art.edu>
2024-04-23 23:44:36 -0700

    Create product_added_to_cart.php

---
+++ IntroSite/styleguide/product_added_to_cart.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Product Item</title>
+<?php include "../parts/meta.php"; ?>
+
+</head>
+<body>
+
+	<?php include "../parts/navbar.php"; ?>
+</header>
+
+
+
+<div class="Container">
+
+	<div class="card soft">
+
+		<h2>You added ____ to your cart!</h2>
+
+		<div class="display-flex">
+
+			<div class="flex-none"><a href="product_list.php">Continue Shopping</a></div>
+
+			<div class="flex-stretch"></div>
+
+			<div class="flex-none"><a href="styleguide/product_cart.php">Go to Cart</a></div>
+
+
+	</div>
+</div>
+
+</div>
+
+	
+</body>
+</html>
