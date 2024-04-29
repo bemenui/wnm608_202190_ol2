cc38a134c4136ea25ba5fbc0b480288d36f861ee
bemenui <bmendoza10@art.edu>
2024-04-28 23:38:15 -0700

    Update product_item.php

--- product_item.php
+++ product_item.php
@@ -1,4 +1,23 @@
-<!DOCTYPE html>
+<?php
+
+		include_once "lib/php/functions.php";
+		
+
+$product = makeQuery(makeConn(),"SELECT * FROM products WHERE id =".$_GET['id'])[0];
+
+$images = explode(",", $product->images);
+
+$image_elements = array_reduce($images,function($r,$o){
+
+	return $r. "<img src='/img/$o'>"''
+
+});
+
+print_p($product);
+
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -10,22 +29,46 @@
 <body>
 
 	<?php include "parts/navbar.php"; ?>
-</header>
+
+
 
 
 
 <div class="Container">
 
-	<div class="card soft">
+	<div class="grid gap">
+	<div class="col-xs-12 col-md-7">
+		<div class="card soft">
 
-		<h2>Product Item</h2>
+			<div class="images-main">
 
-		<p>This is Item # <?=$_GET['id']?></p>
+			<img src="img/<?= $product->thumbnail ?>">
 
+	</div>
+
+	<div class="images-thumbs">
 
 
 	</div>
 </div>
+
+</div>
+
+	<div class="col-xs-12 col-md-5">
+
+		<div class="card soft">
+			
+			<h2 class="product-name"><?= $product->name ?></h2>
+
+			<div class="product-price">&dollar;<?= $product->price ?></div>
+
+
+		</div>
+
+	</div>
+	</div>
+
+	</div>
 
 	
 </body>
