3d90936262a3e25718ce155ffaf77e716ddaf2aa
bemenui <bmendoza10@art.edu>
2024-05-05 11:50:40 -0700

    Update templates.php

--- parts/templates.php
+++ parts/templates.php
@@ -7,11 +7,7 @@ function productListTemplates($r,$o){
 
 	<a class="col-xs-12 col-md-4" href="product_item.php?id=$o->id">
 
-	<div>
-
-			<div class=".productfigure a">
-
-			</div>
+	
 			<figure class="figure product display-flex flex-column">
 			
 			<div class="flex-stretch">
@@ -30,10 +26,93 @@ function productListTemplates($r,$o){
 			
 		</figcaption>
 	</figure>
-		</div>
-
-
+		</a>
 	HTML;
 
 
 }
+
+function selectAmount($amount=1,$total=10){
+
+	$output = "<select name='amount'>";
+	for($i=1;$i<=$total;$i++){
+
+		$output .= "<option ".($i==$amount?"selected":
+			"").">$i</option>";
+	}
+	$output .= "</select>";
+	return $output;
+}
+
+function cartListTemplate($r,$o){
+	$totalfixed = number_format($o->total,2,'.','');
+	$selectamount = selectAmount($o->amount,10);
+	return $r.<<<HTML
+	<div class="display-flex card-section">
+	<div class="flex-none images-thumbs">
+	<img src="img/$o->thumbnail">
+
+	</div>
+
+	<div class="flex-stretch">
+
+	<strong>$o->name ($o->amount)</strong>
+	<form action="styleguide/cart_actions.php?action=delete-cart-item" method="post">
+	<input type="hidden" name="id" value="$o->id">
+	<input type="submit" class="form-button inline" value="Delete" style="font-size:0.8em">
+	</form>
+	</div>
+
+	<div class="flex-none">
+
+	<div>&dollar;$totalfixed</div>
+	<form action="styleguide/cart_actions.php?action=update-cart-item" method="post" onchange="this.submit()">
+
+	<div class="form-select" style="font-size:0.8em">
+
+		$selectamount
+	</div>
+	</form>
+	</div>
+	</div>
+	HTML;
+	}
+
+
+	function cartTotals() {
+
+		$cart = getCartItems();
+
+		$cartprice = array_reduce($cart,function($r,$o){return $r + $o->total;},0);
+
+		$pricefixed = number_format($cartprice,2,'.','');
+
+		$taxfixed = number_format($cartprice * 0.0725,2,'.','');
+
+		$taxedfixed = number_format($cartprice * 1.0725,2,'.','');
+
+		return <<<HTML
+
+					<div class="card-section display-flex">
+						<div class="flex-stretch"><strong>Sub Total</strong></div>
+						<div class="flex-none">&dollar;$pricefixed</div>
+					</div>
+
+					<div class="card-section display-flex">
+
+						<div class="flex-stretch"><strong>Taxes</strong></div>
+						<div class="flex-none">&dollar;$taxfixed</div>
+					</div>
+
+					<div class="card-section display-flex">
+
+						<div class="flex-stretch"><strong>Total</strong></div>
+						<div class="flex-none">&dollar;$taxedfixed</div>
+					</div>
+
+					<div class="card-section">
+						<a href="./lib/styleguide/product_checkout.php" class="form-button">Checkout</a>
+					</div>
+
+					HTML;
+							}
