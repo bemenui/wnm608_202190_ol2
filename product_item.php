a2e476eb1b389747933ede0070e778ddc985fd8e
bemenui <bmendoza10@art.edu>
2024-05-06 01:04:44 -0700

    Update product_item.php

--- product_item.php
+++ product_item.php
@@ -94,6 +94,102 @@ print_p($_SESSION);
 
 
 		
+	<?php
+
+		include_once "lib/php/functions.php";
+		
+
+$product = makeQuery(makeConn(),"SELECT * FROM products WHERE id =".$_GET['id'])[0];
+
+$images = explode(",", $product->images);
+
+$image_elements = array_reduce($images,function($r,$o){
+
+return $r. "<img src='/img/$o'>";
+
+});
+
+
+print_p($_SESSION);
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Product Item</title>
+<?php include "parts/meta.php"; ?>
+
+<script src="js/product_thumbs.js"></script>
+
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+
+
+
+
+<div class="Container">
+
+	<div class="grid gap">
+	<div class="col-xs-12 col-md-7">
+		<div class="card soft">
+
+			<div class="images-main">
+
+			<img src="img/<?= $product->thumbnail ?>">
+
+	</div>
+
+
+	<div class="images-thumbs">
+		<?= $image_elements ?>
+
+
+	</div>
+</div>
+
+</div>
+
+	<div class="col-xs-12 col-md-5">
+
+		<form class="card soft flat" method="post" action="styleguide/cart_actions.php?action=add-to-cart">
+
+			<input type="hidden" name="product-id" value="<?= $product->id ?>">
+
+
+
+			<div class="card-section">
+			
+			<h2 class="product-name"><?= $product->name ?></h2>
+
+			<div class="product-price">&dollar;<?= $product->price ?></div>
+
+		</div>
+
+			<div class="card-section">
+
+				<label for="product-amount" class="form-label">Amount</label>
+
+				<div class="form-select">
+
+					<select id="product-amount" name="product-amount">
+
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+
+					</select>
+				</div>
+
+			</div>
+
+
+		
 		
 
 
