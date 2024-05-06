3456b8e20548653ade54421ae51ebb5cc97eafdf
bemenui <bmendoza10@art.edu>
2024-05-06 01:04:47 -0700

    Update product_list.php

--- product_list.php
+++ product_list.php
@@ -6,6 +6,12 @@
 	<title>Product List</title>
 
 	<?php include "parts/meta.php"; ?>
+
+	<script src="lib/js/functions.js"></script>
+	<script src="js/templates.js"></script>
+	<script src="js/product_list.js"></script>
+
+
 </head>
 <body>
 
@@ -16,43 +22,65 @@
 
 	<div class="card soft">
 
-	
-
-
 		<h2>Product List</h2>
 
-
-		<?php
-
-		include_once "lib/php/functions.php";
-		include_once "parts/templates.php";
-
-		$result = makeQuery(
-			makeConn(), 
+		<div class="form-control">
 			
-			"
-			SELECT * 
-			FROM products
+			<form class="hotdog light" id="product-search">
 
-			ORDER BY date_create DESC
+				<input type="search" placeholder="search products">
 
-			LIMIT 12
-			"
+			</form>
+		</div>
+		<div class="form-control">
 
-		);
+			<div class="card soft">
 
-		echo "<div class='productList grid gap'>",array_reduce($result,'productListTemplates'), "</div>";
+			<div class="display-flex">
+				<div class="flex-stretch">
 
-			?>
 
-</div>
+			<div class="flex-stretch display-flex">
+
+				<button data-filter="category" data-value="" type="button" class="form-button">All</button>
+
+			</div>
+
+			<div class="flex-stretch">
+				<button data-filter="category" data-value="flowers" type="button" class="form-button">flowers</button>
+
+			</div>
+
+				<div class="flex-stretch">
+				<button data-filter="category" data-value="moss" type="button" class="form-button">moss</button>
+
+			</div>
+		</div>
+
+				<div class="flex-none">
+				<div class="form-select">
+
+					<select class="js-sort">
+
+						<option value="1">Newest</option>
+						<option value="2">Oldest</option>
+						
+					</select>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+		
+		<div class='productlist grid gap'></div>
+
+
 </div>
 
 
 	
 </body>
 </html>
-
 
 
 	
