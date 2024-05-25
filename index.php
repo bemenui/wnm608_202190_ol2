fa654423abb2c07fb4cd464b9501eb722dc4260a
bemenui <bmendoza10@art.edu>
2024-05-25 15:03:17 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title> Plant Time </title>
+
+	<meta name="viewport" contents="width=device-width">
+
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+	<link rel="stylesheet" href="lib/css/imagetext.css">
+	<link href="https://www.dafont.com/vogue.font" rel="stylesheet">
+
+
+		
+<header class="navbar">
+
+	<div class="container display-flex">
+		<div class="flex-none"> </div>
+
+		<h1>Plant Time</h1>
+
+	</div>
+		
+		<div class="container flex-stretch"></div>
+		<nav class="flex-none nav">
+		<ul class="container display-flex">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="product_list.php">Plants</a></li>
+			<li><a href="styleguide/product_cart.php">Cart</a></li>
+		</ul>
+		</nav>
+	</div>
+
+
+</div>
+</header>
+
+<body> 
+
+	
+	<div class="grid gap">
+	</div>
+		<div class="col-xs-12 col-md-4">
+			
+			<div class="background">
+		<div class="content-wrapper">
+		<img src="img/green_wall.png" alt="">
+		<div class="text-wrapper">
+		<h1> Want to go Green?</h1>
+				</figure>
+		</div>
+	</div>
+
+		<h3></h3>
+
+
+		<div class="col-xs-12 col-md-4">
+
+			<figure class="figure">
+				<div class="content-wrapper">
+					<div class="hover">
+		<img src="img/plant_wall.png" alt="">
+		
+				<div class="text-wrapper">
+					<h1> What Can Plants Do For You? </h1>
+
+	</figure>
+		</div>
+	</div>
+		<div class="grid"></div>
+
+		<div class="container">
+
+</div>
+
+	<div class="grid gap">
+		<div class="col-xs-12 col-md-4">
+			<figure class="figure product">
+				<div class="content-wrapper">
+		<a href="http://brittneymendoza.com/AAU/WNM608MOD1/product_item.php?id=13"><img src="img/shp.png" alt="">
+
+			<div class="text-wrapper">
+					<p> Succulents</p>
+	</figure></a>
+		</div>
+		<div class="col-xs-12 col-md-4">
+			<figure class="figure product">
+				<div class="content-wrapper">
+		<a href="http://brittneymendoza.com/AAU/WNM608MOD1/product_item.php?id=9"><img src="img/homeflowers.png" alt="">
+
+			<div class="text-wrapper">
+					<p> Flowers</p>
+
+	</figure></a>
+		</div>
+		<div class="col-xs-12 col-md-4">
+			<figure class="figure product">
+				<div class="content-wrapper">
+		<a href="product_list.php"><img src="img/greens2.png" alt="">
+
+			<div class="text-wrapper">
+					<p> Plants</p>
+	
+	</figure></a>
+
+		</figure>
+		
+		</div>
+	</div>
+</div>
+
+<div class="view-window" style="background-image:url('img/plant_window.png')">
+
+</div>
+
+<div class="card soft green">
+
+<p> Dig our shop? Joing our mailing list to get exclusive offers! </p>
+<div class="form-control">
+			
+			<form class="hotdog light" id="product-search">
+
+				<input type="search" placeholder="Email">
+
+			</form>
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+</body>
+
+</html> 
+
+
+
+
+
+
